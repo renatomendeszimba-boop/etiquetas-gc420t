2868738311529cd67f2086a4b97d4590c8b3ad39
renatomendeszimba-boop <renatomendeszimba@gmail.com>
2026-02-08 04:38:47 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ app.get("/logout", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
